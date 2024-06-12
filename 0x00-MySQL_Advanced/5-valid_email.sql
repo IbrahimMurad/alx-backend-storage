9aeae3a8802a880465b2f023a50dc6b6d34ce772
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-06-13 01:23:30 +0300

    Not validated

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,14 @@
+-- creates a trigger that resets the attribute valid_email only when the email has been changed.
+
+DELIMITER $$
+CREATE TRIGGER reset_valid_email
+AFTER UPDATE ON users
+FOR EACH ROW
+BEGIN
+	IF OLD.email != NEW.email
+		UPDATE users
+		SET users.valid_email = 0
+		WHERE users.email=OLD.email
+	END IF
+END;
+DELIMITER ;
