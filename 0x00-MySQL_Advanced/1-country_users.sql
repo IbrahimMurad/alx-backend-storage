b9ef953aba3e440d05ecb9d8dcc03c0686333a0a
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-06-12 15:30:16 +0300

    users from countries

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,9 +1,10 @@
 -- Active: 1702391851621@@127.0.0.1@3306@holberton
 -- create table users
--- with attributes id, email and name
+-- with attributes id, email, name and country (US - CO - TN)
 
 CREATE TABLE IF NOT EXISTS users (
 		id INT AUTO_INCREMENT PRIMARY KEY,
 		email VARCHAR(255) NOT NULL UNIQUE,
-		name VARCHAR(255)
+		name VARCHAR(255),
+		country ENUM('US', 'CO', 'TN') DEFAULT 'US'
 );
