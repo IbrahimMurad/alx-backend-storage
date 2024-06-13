9cbea6b92b3e841ded13684df25b3699b462b81e
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-06-13 06:26:52 +0300

    another try

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -5,8 +5,10 @@ CREATE TRIGGER reset_valid_email
 AFTER UPDATE ON users
 FOR EACH ROW
 BEGIN
-		IF NEW.email != OLD.email AND NEW.valid_email = OLD.valid_email THEN
-			SET users.valid_email = 0;
-		END IF;
+	IF NEW.email != OLD.email AND NEW.valid_email = OLD.valid_email THEN
+		UPDATE users
+		SET users.valid_email = 0
+		WHERE users.email = NEW.email;
+	END IF;
 END$$
 DELIMITER ;
