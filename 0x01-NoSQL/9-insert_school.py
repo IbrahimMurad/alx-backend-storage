1470122f6177c674848c85198664044c55042812
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-06-16 09:18:29 +0300

    pymongo: insert_one

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" defines a function that inserts a new document in a collection based on kwargs """
+
+
+def insert_school(mongo_collection, **kwargs):
+    """ inserts a new document in a collection based on kwargs """
+    the_document = mongo_collection.insert_one(kwargs)
+    return the_document.inserted_id
