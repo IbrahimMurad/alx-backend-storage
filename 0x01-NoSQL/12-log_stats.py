e63b81cc7aaf05969e745f3c667b36b2c0b179a7
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-06-16 14:14:09 +0300

    pymongo: name = main

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -3,22 +3,23 @@
 from pymongo import MongoClient
 
 
-client = MongoClient('mongodb://127.0.0.1:27017')
-nginx_collection = client.logs.nginx
-num_of_logs = nginx_collection.count_documents({})
-GET_methods = nginx_collection.count_documents({"method": "GET"})
-POST_methods = nginx_collection.count_documents({"method": "POST"})
-PUT_methods = nginx_collection.count_documents({"method": "PUT"})
-PATCH_methods = nginx_collection.count_documents({"method": "PATCH"})
-DELETE_methods = nginx_collection.count_documents({"method": "DELETE"})
-status_checked = nginx_collection.count_documents(
-        {"method": "GET", "path": "/status"}
-    )
-print("{} logs".format(num_of_logs))
-print("Methods:")
-print("    method GET: {}".format(GET_methods))
-print("    method POST: {}".format(POST_methods))
-print("    method PUT: {}".format(PUT_methods))
-print("    method PATCH: {}".format(PATCH_methods))
-print("    method DELETE: {}".format(DELETE_methods))
-print("{} status check".format(status_checked))
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
+    num_of_logs = nginx_collection.count_documents({})
+    GET_methods = nginx_collection.count_documents({"method": "GET"})
+    POST_methods = nginx_collection.count_documents({"method": "POST"})
+    PUT_methods = nginx_collection.count_documents({"method": "PUT"})
+    PATCH_methods = nginx_collection.count_documents({"method": "PATCH"})
+    DELETE_methods = nginx_collection.count_documents({"method": "DELETE"})
+    status_checked = nginx_collection.count_documents(
+            {"method": "GET", "path": "/status"}
+        )
+    print("{} logs".format(num_of_logs))
+    print("Methods:")
+    print("    method GET: {}".format(GET_methods))
+    print("    method POST: {}".format(POST_methods))
+    print("    method PUT: {}".format(PUT_methods))
+    print("    method PATCH: {}".format(PATCH_methods))
+    print("    method DELETE: {}".format(DELETE_methods))
+    print("{} status check".format(status_checked))
