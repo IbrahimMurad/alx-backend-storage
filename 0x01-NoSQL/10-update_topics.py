7b5df6bd6846e1cf1e008627c9e8d5d3e7f6683c
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-06-16 09:43:01 +0300

    pymongo: update

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -4,4 +4,5 @@
 
 def update_topics(mongo_collection, name, topics):
     """ changes all topics of a school document based on the name """
-    mongo_collection.update_one({"name" : name}, {"$set": {"topics": topics}}, upsert=True)
+    for school in mongo_collection.find({"name": name}):
+        school.update({"topics": topics})
