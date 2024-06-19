3336ab156e4feece990ff05500e5224c9c1e9494
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-06-19 15:42:51 +0300

    wrapper

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,6 +4,19 @@ import redis
 from uuid import uuid4
 from typing import Callable
 from typing import Union, Optional
+from functools import wraps
+
+
+def count_calls(f: Callable) -> Callable:
+    """ counts the number of times a method is called """
+    @wraps(f)
+    def wrapper(self, *args, **kwargs):
+        """ wrapper function """
+        if not self.get(f.__qualname__):
+            self._redis.set(f.__qualname__, 0)
+        self._redis.incr(f.__qualname__)
+        return f(self, *args, **kwargs)
+    return wrapper
 
 
 class Cache:
@@ -16,13 +29,14 @@ class Cache:
         )
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """ takes data and stores it in redis with a random generated key """
         key = str(uuid4())
         self._redis.set(key, data)
         return key
 
-    def get(self, key: str, fn: Optional[Callable]) -> Optional[Union[str, bytes, int, float]]:
+    def get(self, key: str, fn: Optional[Callable] = None) -> Optional[Union[str, bytes, int, float]]:
         """ gets the data from redis and uses fn to decode it """
         value = self._redis.get(key)
         if value:
