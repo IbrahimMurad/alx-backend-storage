5614bf7b7def0a21a9e533e7e8d703a97e3dd4d1
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-06-20 05:53:54 +0300

    caching the results too

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -9,16 +9,13 @@ import redis
 def get_page(url: str) -> str:
     """ uses the requests module to obtain
     the HTML content of a particular URL and returns it. """
-    r = get(url)
-    if r.status_code != 200:
-        return None
-    red = redis.Redis(
-            host='localhost',
-            port=6379
-        )
-    key = "count:{{{}}}".format(url)
-    if not red.exists(key):
-        red.set(key, 0)
-    red.incr(key)
-    red.expire(name=key, time=10)
-    return r.text
+    red = redis.Redis()
+    count_key = "count:{}".format(url)
+    result_key = "result:{}".format(url)
+    red.incr(count_key)
+    if red.exists(result_key):
+        return red.get(result_key).decode('utf-8')
+    result = get(url).text
+    red.set(result_key, result)
+    red.expire(result_key, 10)
+    return result
