8c53bd0dc54aec4ae7f13c713f58fc698ad68b2f
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-06-20 05:59:23 +0300

    with decorator

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -4,18 +4,29 @@ stores how many requests a url is made
 and cache the result with an expiration time of 10 seconds. """
 from requests import get
 import redis
+from typing import Callable
 
 
+def caching_decorator(method: Callable) -> Callable:
+    """ get_page wrapper that caches the result
+    with an expiration time of 10 seconds. """
+    def wrapper(url: str) -> str:
+        """ wrapper function """
+        red = redis.Redis()
+        count_key = "count:{}".format(url)
+        result_key = "result:{}".format(url)
+        red.incr(count_key)
+        if red.exists(result_key):
+            return red.get(result_key).decode('utf-8')
+        result = method(url)
+        red.setex(result_key, 10, result)
+        return result
+    return wrapper
+
+
+@caching_decorator
 def get_page(url: str) -> str:
     """ uses the requests module to obtain
     the HTML content of a particular URL and returns it. """
-    red = redis.Redis()
-    count_key = "count:{}".format(url)
-    result_key = "result:{}".format(url)
-    red.incr(count_key)
-    if red.exists(result_key):
-        return red.get(result_key).decode('utf-8')
-    result = get(url).text
-    red.set(result_key, result)
-    red.expire(result_key, 10)
-    return result
+    r = get(url)
+    return r.text
